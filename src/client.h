f73622f2b64aa15a7a9addca4ad8c6959e65997b
Simon Hsu <simonizerlol@gmail.com>
2018-09-06 15:22:40 -0400

    update to match master commit

--- src/client.h
+++ src/client.h
@@ -1,3 +1,4 @@
+/*
 #ifndef SRC_ClIENT_H_
 #define SRC_ClIENT_H_
 
@@ -57,3 +58,32 @@ class Player {
   std::shared_ptr<PlayerClient> Client;
 };
 #endif  // SRC_ClIENT_H_
+
+*/
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include "object.h"
+#include "constant.h"
+#include "game_state.h"
+#include "input.h"
+
+class GameState;
+class Client : public Object, public std::enable_shared_from_this<Client> {
+    public:
+        Client(
+            const std::shared_ptr<GameState> &gameState,
+            const std::string id,
+            const std::string textureId,
+            SDL2pp::Point pos
+        );
+        void Update();
+        void HandleInput(const std::shared_ptr<Input> & input);
+
+        void Fire();
+        
+
+    private:
+        int heath;
+};
+#endif // CLIENT_H
