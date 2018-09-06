3a79c2dd64e4663d3712bce18e7921cfb7d3d6d8
Simon Hsu <simonizerlol@gmail.com>
2018-09-06 15:25:09 -0400

    update to match master commit

--- src/input.cpp
+++ src/input.cpp
@@ -1,6 +1,19 @@
+/*
 #include "input.h"
 #include "SDL2/SDL.h"
 
 InputManager::InputManager() {
   keys = SDL_GetKeyboardState(nullptr);
+}
+*/
+#include "input.h"
+
+Input::Input()
+{
+    keystate = SDL_GetKeyboardState(nullptr);
+}
+
+bool Input::IsPressed(int key)
+{
+    return keystate[key];
 }
