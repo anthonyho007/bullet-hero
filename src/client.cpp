59aba7a1396ce9368d62f6cb0e11e58f26f7f355
Simon Hsu <simonizerlol@gmail.com>
2018-09-06 23:45:09 -0400

    implemented client keypress direction

--- src/client.cpp
+++ src/client.cpp
@@ -1,5 +1,5 @@
-
 #include "client.h"
+
 Client::Client(
             const std::shared_ptr<GameState> & gameState,
             const std::string id,
@@ -16,6 +16,7 @@ Client::Client(
     id(id),
     heath(5)
 {}
+
 void Client::Update() {
     renderer->Copy(
         *assetsManager->GetTexture(textureId),
@@ -29,6 +30,21 @@ void Client::Update() {
 void Client::HandleInput(const std::shared_ptr<Input> & input) {
     if (input->IsPressed(SDL_SCANCODE_SPACE)) {
         Fire();
+    }
+    if (input->IsPressed(SDL_SCANCODE_UP) || input->IsPressed(SDL_SCANCODE_W) || input->IsPressed(SDL_SCANCODE_K)) {
+      pos.y--;
+    }
+
+    if (input->IsPressed(SDL_SCANCODE_DOWN) || input->IsPressed(SDL_SCANCODE_S) || input->IsPressed(SDL_SCANCODE_J)) {
+      pos.y++;
+    }
+
+    if (input->IsPressed(SDL_SCANCODE_LEFT) || input->IsPressed(SDL_SCANCODE_A) || input->IsPressed(SDL_SCANCODE_H)) {
+      pos.x--;
+    }
+
+    if (input->IsPressed(SDL_SCANCODE_RIGHT) || input->IsPressed(SDL_SCANCODE_D) || input->IsPressed(SDL_SCANCODE_L)) {
+      pos.x++;
     }
 }
 
